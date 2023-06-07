63bc87c62586476f80f5d4128e5f1cf7aef4de54
Cava <paul_adrianll@yahoo.com>
2023-06-07 15:02:35 +0300

    form fixed

--- views/login.php
+++ views/login.php
@@ -1,7 +1,10 @@
 <div class="bg-light p-5 text-center">
-    <input type="email" class="form-control m-2 username" placeholder="Username">
-    <input type="password" class="form-control m-2 password" placeholder="Password">
-    <button class="btn btn-success" onclick="login()">Login</button>
+    <form action="">
+
+        <input type="email" class="form-control m-2 username" placeholder="Username">
+        <input type="password" class="form-control m-2 password" placeholder="Password">
+        <button class="btn btn-success" type="submit" onclick="login()">Login</button>
+    </form>
     <p class="response text-danger"></p>
 </div>
 <div id="div_session_write"></div>
